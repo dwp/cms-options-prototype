4350e0a330fe1e8f00501662808b6cef2f7abf7c
RossDavidBrown <ross.brown@engineering.digital.dwp.gov.uk>
2025-08-18 15:09:28 +0100

    Update to kit v13
    GitOrigin-RevId: be32d9df785b4635dfe5b75d259f5ebbe5f5c576

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,13 @@
-const express = require('express')
-const router = express.Router()
+//
+// For guidance on how to create routes see:
+// https://prototype-kit.service.gov.uk/docs/create-routes
+//
 
-// Add your routes here - above the module.exports line
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+
+// Add your routes here
+
 
 // copy the const name and the file path to new version
 const radioButtonRedirect = require('radio-button-redirect')
@@ -60,4 +66,3 @@ router.use(urnerror);
 router.use(g4s1);
 router.use(g4s2);
 
-module.exports = router
